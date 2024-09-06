734bc8b354d9188330361a0357900461511e84e0
milanesadeternera <facundojcolombo@gmail.com>
2024-09-06 15:46:36 -0300

    init filtrado personajes

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 URL="https://dragonball-api.com/api/characters"
+//URL="data.json"
 let personajes = [];
 
 document.addEventListener("DOMContentLoaded",async ()=>{
-    document.getElementById("buscar").addEventListener("click", buscar);
+    document.getElementById("buscar").addEventListener("input", buscar);
 
+
+    //cargo los personajes
     let fin = false;   
     while(!fin){
         let datos = await getJSONData(URL);
@@ -42,30 +45,36 @@ let getJSONData =  async function(url){
     });
 }
 
-function displayData(values){
+function displayData(values, type){
+    console.log(values);
     let contenedor=document.getElementById("contenedor");
     data="";
-    values.forEach(element => {
-        data +=`<div class="card col-md-3">
+    //values.forEach(element => {
+    for(let i=0;i<=10 ; i++){
+        console.log("i vale:"+i)
+        element = values[i];
+        data +=`<div class="card col-md-3 mt-3">
                         <div class="card-body">
-                            <h5 class="card-title">${element.name}</h5>
+                            <div class="image-container img-thumbnail mb-1">
+                                <img class="image" src=${element.image}></image>
+                            </div>
+                            <h4 class="card-title fw-bold">${element.name}</h4>
                             <p class="card-text descripcion">${element.description}</p>
                         </div>
                     </div>`
+    }
 
-    });
+    //});
     contenedor.innerHTML=data;
 }
 
 function buscar(){
-    let patron = document.getElementById("clave").value;
-    let items = document.getElementsByClassName("personaje");
-    for (let item of items) {
-        if(item.innerHTML.match(patron)==null){
-            item.style.display="none";
-        }else{
-            item.style.display="";
-        }
-    }    
+    let patron = document.getElementById("buscar").value;
+
+    //Busco nombre de personaje
+    let match = personajes.filter(personaje => !personaje.name.toLocaleLowerCase().search(patron.toLocaleLowerCase()))
+
+    //console.log(match)
+    displayData(match, "filter");
 
 }
